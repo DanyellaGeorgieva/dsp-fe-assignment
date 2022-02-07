3f70a767c1f9236f7163cb33046faa3b1a201786
Daniela Georgieva <daniela@next-dc.com>
2022-02-07 22:53:26 +0200

    git goodnight

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,6 @@
-
-
 const swiper = new Swiper('.swiper', {
-    // Navigation arrows
+  spaceBetween: 80,
+  loop: true,
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
